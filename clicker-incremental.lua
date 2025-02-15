4c1d08f462417b23fa0705c296104b97dea38b87
mehhovcki <145855444+mehh0vcki@users.noreply.github.com>
2025-02-15 23:45:38 +0300

    Update clicker-incremental.lua

--- clicker-incremental.lua
+++ clicker-incremental.lua
@@ -1,107 +1,339 @@
-_G.ClickDetector = true
-_G.CollectBobux = true
-_G.CollectSupply = true
-_G.GhostBobux = true
-_G.CollectGrass = true
+-- variables 
+local ClickDetectorMain =false
+local ClickDetectorGold =false
 
-local clickdetector = Instance.new("ClickDetector", workspace)
-local success, error = pcall(function()
-    fireclickdetector(clickdetector, 0)
+local CollectDefaultBobux = false
+local CollectRareBobux =    false
+local CollectRainyBobux =   false
+local CollectGhostBobux =   false
+local CollectGoldenBobux =  false
+local CollectBobuxSupply =  false
+
+local AutoBobuxExchange =false
+local ChoosenUpgradeMain =  ""
+local ChoosenUpgradeGrass = ""
+local ChoosenUpgradeGolden =""
+
+-- required
+local player = game.Players.LocalPlayer
+local function teleportStuff(part)
+    -- making this function so i don't write anchor + collision every time. makes script more readable, but make me get called gpt user 😨😨
+    part.CFrame = player.Character.Head.CFrame
+    part.Anchored = true
+    part.CanCollide = false
+end
+
+-- checks
+local clickDetector = Instance.new("ClickDetector", workspace)
+local CDSuccess, error = pcall(function()
+    fireclickdetector(clickDetector, math.random(1, 3))
 end)
-clickdetector:Destroy()
+clickDetector:Destroy()
 
-if not success then
-    warn("fireclickdetector() is not supported by your exploit. Auto-Clicker will not work.")
+local DiscordUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/mehh0vcki/scripts/refs/heads/main/static/library.lua"))("script by mehhovcki :3")
+local UIWindow = DiscordUI:Window("mehhovcki' script™ | Discord UI Library")
+local MainTab = UIWindow:Server("[7.5] Clicker Incremental", "rbxassetid://16742508014")
+local MainChannel = MainTab:Channel("Main")
+
+if not CDSuccess then
+    DiscordUI:Notification(
+        "Error",
+        "Tests showed us, that fireclickdetector() may not work for your exploit.",
+        "Ok!"
+    )
 end
-
-local function fireClickDetector()
-    local clickerPart = Workspace.Clicker.ClickDetector
-    while _G.ClickDetector and wait(0.1) do
-        fireclickdetector(clickerPart, 0.69420)
-    end
-end
-
-local function teleportBobux()
-    local bobuxFolder = Workspace.BobuxFolder
-    local character = game.Players.LocalPlayer.Character
-    while _G.CollectBobux and wait(0.1) do
-        for _, bobux in ipairs(bobuxFolder:GetChildren()) do
-            bobux.CFrame = character.Head.CFrame
-            -- firetouchinterest(bobux, bobux.TouchInterest, 0)
-        end
-
-        local rareBobux = workspace:FindFirstChild("RareBobux")
-        if rareBobux ~= nil then
-            rareBobux.CFrame = character.Head.CFrame
-            -- firetouchinterest(rareBobux, rareBobux.TouchInterest, 0)
-        end
-
-        local rainBobux = workspace:FindFirstChild("RainyBobux")
-        if rainBobux ~= nil then
-            rainBobux.CFrame = character.Head.CFrame
+MainChannel:Label("= Autoclickers = ")
+MainChannel:Toggle(
+    "Default Bobux Autoclick",
+    false,
+    function(bool)
+        local Bobux = workspace:FindFirstChild("Clicker")
+        ClickDetectorMain = bool
+       
+        while ClickDetectorMain and wait(0.1) do
+            fireclickdetector(Bobux.ClickDetector, math.random(1, 3))
         end
     end
-end
+)
 
-local function enableBobuxSupply()
-    local character = game.Players.LocalPlayer.Character
-    while _G.CollectSupply and wait(1) do
-        local bobuxSupply = workspace:FindFirstChild("BobuxSupply")
-        if bobuxSupply then
-            fireclickdetector(bobuxSupply.ClickDetector, 2)
-            wait(2)
+MainChannel:Toggle(
+    "Golden Bobux Autoclick",
+    false,
+    function(bool)
+        local GoldenBobux = workspace:FindFirstChild("GBobuxClicker")
+        ClickDetectorGold = bool
+        
+        while ClickDetectorGold and wait(0.1) do
+            fireclickdetector(GoldenBobux.ClickDetector, math.random(1, 3))
+        end
+    end
+)
 
-            local LotOfRareRobux = workspace.LotOfRareBobux
-            for _, item in ipairs(LotOfRareRobux:GetChildren()) do
-                item.CFrame = character.Head.CFrame
+MainChannel:Label("= Exchange =")
+MainChannel:Button(
+    "Buy 1 Golden Bobux (2,000B/1GB)",
+    function()
+        local buyGoldenBobux = workspace:FindFirstChild("BuyGoldenBobux")
+        local buyOne = buyGoldenBobux.Button1
+        fireclickdetector(buyOne.ClickDetector, math.random(1, 3))
+    end
+)
+
+MainChannel:Button(
+    "Buy 5 Golden Bobux (10,000B/5GB)",
+    function()
+        local buyGoldenBobux = workspace:FindFirstChild("BuyGoldenBobux")
+        local buyFive = buyGoldenBobux.Button2
+        fireclickdetector(buyFive.ClickDetector, math.random(1, 3))
+    end
+)
+
+local ToggleChannels = MainTab:Channel("Toggles")
+ToggleChannels:Label("= Collectables/Clickable = ")
+ToggleChannels:Toggle(
+    "Collect Bobux",
+    false,
+    function(bool)
+        CollectDefaultBobux = bool
+        local bobuxFolder = workspace:FindFirstChild("BobuxFolder")
+        if bobuxFolder then
+            while CollectDefaultBobux and wait(0.1) do
+                for _, bobux in ipairs(bobuxFolder:GetChildren()) do
+                    teleportStuff(bobux)
+                end
             end
-            LotOfRareRobux:Destroy()
         end
     end
-end
+)
 
-local function collectGhostBux()
-    local character = game.Players.LocalPlayer.Character
-    while _G.GhostBobux and wait(1) do
-        local ghostBobux = workspace:FindFirstChild("GhostBobux")
-        if ghostBobux then
-            ghostBobux.CFrame = character.Head.CFrame
-        end
-        -- workspace.GhostBobux
-    end
-end
-
-local function collectGrassBux()
-    local backpack = game.Players.LocalPlayer.Backpack
-    local character = game.Players.LocalPlayer.Character
-    while _G.CollectGrass and wait(.1) do
-        local grassBux = workspace:FindFirstChild("GrassBux")
-        local grassAccepter = workspace:FindFirstChild("GrassAccepter")
-
-        if grassBux then
-            grassBux.Handle.CFrame = character.Head.CFrame
-            -- task.wait(2)
-            -- if backpack:FindFirstChild("GrassBux") then
-            --     character.Humanoid:EquipTool(backpack.GrassBux)
-            --     task.wait(0.5)
-            --     character.GrassBux.BuxTouchPart.CFrame = grassAccepter.CFrame
-            -- end
+ToggleChannels:Toggle(
+    "Collect Rare Bobux",
+    false,
+    function(bool)
+        CollectRareBobux = bool
+        while CollectRareBobux and wait(0.1) do
+            local rareBobux = workspace:FindFirstChild("RareBobux")
+            if rareBobux then
+                teleportStuff(rareBobux)
+            end
         end
     end
-end
+)
 
-task.spawn(function()
-    fireClickDetector()
-end)
-task.spawn(function()
-    teleportBobux()
-end)
-task.spawn(function()
-    enableBobuxSupply()
-end)
-task.spawn(function()
-    collectGrassBux()
-end)
-task.spawn(function()
-    collectGhostBux()
-end)
+ToggleChannels:Toggle(
+    "Collect Gold Bobux",
+    false,
+    function(bool)
+        CollectGoldenBobux = bool
+        while CollectGoldenBobux and wait(.1) do
+           local goldenBobux = workspace:FindFirstChild("GoldenBobux")
+            if goldenBobux then
+                teleportStuff(goldenBobux)
+            end
+        end
+    end
+)
+
+ToggleChannels:Label("= Collectables/Spawnable =")
+ToggleChannels:Toggle(
+    "Collect Rainy Bobux",
+    false,
+    function(bool)
+        CollectRainyBobux = bool
+        while CollectRainyBobux and wait(.1) do
+            local rainyBobux = workspace:FindFirstChild("RainyBobux")
+            if rainyBobux then
+               teleportStuff(rainyBobux) 
+            end
+        end
+    end
+)
+
+ToggleChannels:Toggle(
+    "Collect Ghost Bobux",
+    false,
+    function(bool)
+        CollectGhostBobux = bool
+        while CollectGhostBobux and wait(.1) do
+            local ghostBobux = workspace:FindFirstChild("GhostBobux")
+            if ghostBobux then
+                teleportStuff(ghostBobux)
+            end
+        end
+    end
+)
+
+ToggleChannels:Toggle(
+    "Collect Bobux Supply",
+    false,
+    function(bool)
+        CollectBobuxSupply = bool
+        while true and wait() do
+            if workspace:FindFirstChild("LotOfRareRobux") then
+                local items = workspace:FindFirstChild("LotOfRareRobux"):GetChildren()
+                if #items == 0 then
+                    workspace:FindFirstChild("LotOfRareRobux"):Destroy()
+                end
+            else
+                break
+            end
+        end
+
+        while CollectBobuxSupply and wait(1) do
+            local bobuxSupply = workspace:FindFirstChild("BobuxSupply")
+            if bobuxSupply then
+                fireclickdetector(bobuxSupply.ClickDetector, math.random(1, 3))
+                task.wait(2)
+                local LotOfRareRobux = workspace.LotOfRareBobux
+
+                if LotOfRareRobux then
+                    for _, item in ipairs(LotOfRareRobux:GetChildren()) do
+                        teleportStuff(item)
+                    end
+                end
+            end
+        end
+    end
+)
+
+local PurchaseChannel = MainTab:Channel("Purchases")
+PurchaseChannel:Label("= Upgrade/Main =")
+PurchaseChannel:Dropdown(
+    "Choose Upgrade",
+    {
+        "BobuxUpgrade1",
+        "ColdownUpgrade",
+        "RareBobuxUpgrade",
+        "MultiplierBobux1",
+        "PartsLimitUpgrade",
+        "ConveyorSpeedUpgrade",
+        "AutoBobux",
+        "AutoBobuxUpgrade"
+    },
+    function(option)
+        ChoosenUpgradeMain = option
+    end
+)
+
+PurchaseChannel:Button(
+    "Purchase Upgrade",
+    function()
+        local upgrades = workspace:FindFirstChild("Upgrades")
+        if upgrades then
+            local upgrade = upgrades:FindFirstChild(ChoosenUpgradeMain)
+            if upgrade then
+                fireclickdetector(upgrade.Button.ClickDetector, math.random(1, 3))
+            end
+        end
+    end
+)
+
+PurchaseChannel:Label("= Upgrade/Grass =")
+PurchaseChannel:Dropdown(
+    "Choose Upgrade",
+    {
+        "GrassSpawnUpgrade",
+        "GrassUpgrade",
+        "GrassGrowUpgrade",
+        "BobuxUpgrade3",
+        "GrassLimitUpgrade",
+        "GoldenMultiplier2",
+        "MultiplierBobux3",
+    },
+    function(option)
+        ChoosenUpgradeGrass = option
+    end
+)
+
+PurchaseChannel:Button(
+    "Purchase Upgrade",
+    function()
+        local upgrades = workspace:FindFirstChild("Upgrades")
+        if upgrades then
+            local upgrade = upgrades:FindFirstChild(ChoosenUpgradeGrass)
+            if upgrade then
+                fireclickdetector(upgrade.Button.ClickDetector, math.random(1, 3))
+            end
+        end
+    end
+)
+
+PurchaseChannel:Label("= Upgrade/Golden =")
+PurchaseChannel:Dropdown(
+    "Choose Upgrade",
+    {
+        "GoldenColdownUpgrade",
+        "GoldenBobuxUpgrade",
+        "ClicksNeededUpgrade",
+        "BobuxUpgrade2",
+        "MultiplierBobux2",
+        "GoldenMultiplier1",
+        "BobuxUpgrade5",
+        "AutoBobuxUpgrade2",
+    },
+    function(option)
+        ChoosenUpgradeGolden = option
+    end
+)
+
+PurchaseChannel:Button(
+    "Purchase Upgrade",
+    function()
+        local upgrades = workspace:FindFirstChild("Upgrades")
+        if upgrades then
+            local upgrade = upgrades:FindFirstChild(ChoosenUpgradeGolden)
+            print("upgrade: ", upgrade, " option: golden")
+            if upgrade then
+                fireclickdetector(upgrade.Button.ClickDetector, math.random(1, 3))
+            end
+        end
+    end
+)
+
+local CreditTab = UIWindow:Server("[v0.2] Credits", "rbxassetid://110969324988843")
+local CreditChannel = CreditTab:Channel("@mehhovcki")
+CreditChannel:Label("# created by @mehhovcki")
+CreditChannel:Label("you can find scripts here:")
+CreditChannel:Label("> https://scriptblox.com/ (Update-7.5-Clicker-Incremental...)")
+CreditChannel:Label("> https://github.com/ (mehh0vcki/scripts)")
+CreditChannel:Label("if you got this script from anywhere else,")
+CreditChannel:Label("someone made a dollar out of you.")
+
+CreditChannel:Button(
+    "Copy Script Page",
+    function()
+        setclipboard("https://scriptblox.com/script/Update-7.5-Clicker-Incremental-SIMPLE-SCRIPT-For-29299")
+        DiscordUI:Notification(
+            "Copied Script Page",
+            "Succesfully copied Script Page.",
+            "Ok!"
+        )
+    end
+)
+
+CreditChannel:Button(
+    "Copy GitHub Page",
+    function()
+        setclipboard("https://github.com/mehh0vcki/scripts")
+        DiscordUI:Notification(
+            "Copied GitHub Page",
+            "Succesfully copied GitHub Page.",
+            "Ok!"
+        )
+    end
+)
+
+CreditChannel:Label("best support you can give is:")
+CreditChannel:Label("* like on scriptblox.com")
+CreditChannel:Label("* star on github")
+
+local VersionChannel = CreditTab:Channel("Version")
+VersionChannel:Label("[ Update Log for version #0.2 ]")
+VersionChannel:Label("* bobux > gold bobux echange")
+VersionChannel:Label("* seperated bobux collection")
+VersionChannel:Label("* added ability to buy upgrades for rooms:")
+VersionChannel:Label("  > main")
+VersionChannel:Label("  > golden")
+VersionChannel:Label("  > grass")
+VersionChannel:Label("* using Discord UI! :D")
+-- mehhovcki was here ;3
